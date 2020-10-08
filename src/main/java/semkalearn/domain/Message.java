92d1d0141659abe50c19e0255f13c2b96d8e410d
Andrey Semka <k9nz00@yandex.ru>
2020-10-08 21:09:23 +0300

    добавил простой фронт

---
+++ src/main/java/semkalearn/domain/Message.java
@@ -0,0 +1,19 @@
+package semkalearn.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+
+//@Entity
+//@Table
+//@Data
+//@EqualsAndHashCode(of = {"id"})
+public class Message {
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private long id;
+//
+//    private String test;
+}
---
+++ src/main/java/semkalearn/repo/MessageRepo.java
@@ -0,0 +1,7 @@
+package semkalearn.repo;
+
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import semkalearn.domain.Message;
+
+public interface MessageRepo {
+}
